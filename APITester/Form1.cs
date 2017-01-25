5aee613d2b50e1a2f02d1fe85613a2a91d96b0be
zeroyxz <peterworlin@hotmail.com>
2017-01-25 15:33:05 +0000

    Add APITester Windows Form Project

---
+++ APITester/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace APITester
+{
+    public partial class Form1 : Form
+    {
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        Uri redirectUri = new Uri(ConfigurationManager.AppSettings["ida:RedirectUri"]);
+
+        private static string authority = String.Format(aadInstance, tenant);
+
+        private static string apiResourceId = ConfigurationManager.AppSettings["ApiResourceId"];
+        private static string apiBaseAddress = ConfigurationManager.AppSettings["ApiBaseAddress"];
+
+        private AuthenticationContext authContext = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //You need the async attribute on the button click because you are invoking 
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            authContext = new AuthenticationContext(authority);
+
+            //UserCredential cred = new UserCredential() - this is obsolete and replaced with UserPasswordCredential
+            //UserPasswordCredential cred = new UserPasswordCredential(); - this expects a usernam and password though which we dont know
+            //ClientCredential cred = new ClientCredential();//This nexpects a client_id and client_secret - this is for a two-legged approach where we are not impersonating a user to make the request to the web api we are using the identity of the AAD registered application
+
+            //The AcquireToken method is only within version 2.28.2 of the ADAL
+            //To get the rght version using package manager:
+            //--Get-Project APITester | Install-package Microsoft.IdentityModel.Clients.ActiveDirectory -Version 2.28.2
+            AuthenticationResult authResult = authContext.AcquireToken(apiResourceId, clientId, redirectUri);
+
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+
+            //HttpResponseMessage response = await client.GetAsync(apiBaseAddress + "employees");
+            HttpResponseMessage response = await client.GetAsync(apiBaseAddress + "employees");
+
+
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            MessageBox.Show(responseString);
+        }
+    }
+}
---
+++ APITester/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("APITester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("APITester")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("626f90c0-c630-4507-9cf0-4b5827f6d9c6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
